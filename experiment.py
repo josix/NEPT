f939212832d8314004be269f267229b17cd6b2ef
WilsonWang <wilson8507@gmail.com>
2018-01-02 03:06:48 +0800

    Update recommendation_list items

--- experiment.py
+++ experiment.py
@@ -48,7 +48,7 @@ def recommendation(query, item_vertex_embedding, item_detail_map=None):
         print("{} Recommendation: {}".format(index, recommendation))
         # show detail
         print(item_detail_map[recommendation[1]])
-    return recommendation_list[1:11]
+    return list(map(lambda x: x[1], recommendation_list[1:11]))
 
 def cosine(v1, v2):
     numerator = 0
@@ -78,7 +78,7 @@ def popularity_recommendation(query, recommendation_list, item_detail_map=None):
     print(item_detail_map[query])
     for index, recommendation in enumerate(recommendation_list):
         print("{} Recommendation: {}".format(index, recommendation))
-        print(item_detail_map[recommendation[1]])
+        print(item_detail_map[recommendation])
     return recommendation_list
 
 
@@ -90,15 +90,15 @@ if __name__ == "__main__":
 
     user_watch_list, user_set, item_set = load_raw_data('../kktix/netWorkDataAllState/training_data.data', "../kktix/netWorkDataAllState/testing_data.data")
     # model_recommendation
-    user_vertex_embedding, item_vertex_embedding = load_embedding('../kktix/netWorkDataAllState/rep.hpe', user_set, item_set)
+    # user_vertex_embedding, item_vertex_embedding = load_embedding('../kktix/netWorkDataAllState/rep.hpe', user_set, item_set)
 
     # popularity_recommendation
-    # command = "cat ../kktix/netWorkDataAllState/etNet.test ../kktix/netWorkDataAllState/etNet.train\
-            # | awk 'BEGIN{item[$2]=0}{item[$2] = item[$2] + 1}END{for(i in item){print i, item[i]}}'"
-    # result = subprocess.check_output(command, shell=True).decode('utf-8').split('\n')
-    # popularity_list = [(int(i.split()[1]), i.split()[0]) for i in result if len(i.split()) == 2]
-    # popularity_list.sort(reverse=True)
-    # popularity_list = popularity_list[:10]
+    command = "cat ../kktix/netWorkDataAllState/etNet.test ../kktix/netWorkDataAllState/etNet.train\
+            | awk 'BEGIN{item[$2]=0}{item[$2] = item[$2] + 1}END{for(i in item){print i, item[i]}}'"
+    result = subprocess.check_output(command, shell=True).decode('utf-8').split('\n')
+    popularity_list = [(int(i.split()[1]), i.split()[0]) for i in result if len(i.split()) == 2]
+    popularity_list.sort(reverse=True)
+    popularity_list = list(map(lambda x: x[1], popularity_list[:10]))
 
     # Read experiment data
     with open('../kktix/netWorkDataAllState/experimentQuery.data') as fin:
@@ -113,13 +113,13 @@ if __name__ == "__main__":
             user, item = line.strip().split()
             print('query user:', user)
             # model_recommendation
-            recommendation_list = recommendation(item, item_vertex_embedding, item_detail_map)
+            # recommendation_list = recommendation(item, item_vertex_embedding, item_detail_map)
 
             # random_recommendation
             # recommendation_list = random_recommendation(item, item_set, item_detail_map)
 
             # popularity_recommendation
-            # recommendation_list = popularity_recommendation(item, popularity_list, item_detail_map)
+            recommendation_list = popularity_recommendation(item, popularity_list, item_detail_map)
 
             if not recommendation_list:
                 print("No existed query embedding in training data.")
@@ -127,7 +127,7 @@ if __name__ == "__main__":
                 continue
 
             # scoring
-            machingNum = len(set(map(lambda x: x[1], recommendation_list)) & set(user_watch_list[user]))
+            machingNum = len(set(recommendation_list) & set(user_watch_list[user]))
             precision = machingNum/10
             recall = machingNum/len(user_watch_list[user])
             print('precision@10: {} recall@10: {}'.format(precision, recall))
@@ -137,8 +137,7 @@ if __name__ == "__main__":
 
                 ranked_precision = 0
                 ranked_machingNum = 0
-                for index, result in enumerate(recommendation_list):
-                    temp, item = result
+                for index, item in enumerate(recommendation_list):
                     if item in user_watch_list[user]:
                         ranked_machingNum += 1
                         ranked_precision += ranked_machingNum / (index + 1)
@@ -151,4 +150,5 @@ if __name__ == "__main__":
 
         print('# of queries: {}'.format(count))
         print('# of queries(precision > 0): {}'.format(maching_count))
-        print('MAP: {}'.format(total_avep / maching_count))
+        if maching_count:
+            print('MAP: {}'.format(total_avep / maching_count))
