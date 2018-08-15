434f923cfe850a3a6b92bc521adaa474218f6f59
WilsonWang <wilson8507@gmail.com>
2018-08-15 19:18:11 +0800

    Update vsm_propagation
    * Add weight_func to embedding_propgation function
    * Update weight function to vsm propgation
    * Add idf to query to vsm
    * Show the weighting list of propagated events

--- experiment/vsm_propagation.py
+++ experiment/vsm_propagation.py
@@ -4,6 +4,7 @@ to the unseen item entity.
 """
 import json
 import argparse
+from math import pi, acos
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import jieba
@@ -23,7 +24,7 @@ PARSER.add_argument("corpus_file",
 ARGS = PARSER.parse_args()
 UNSEEN_EVENTS_FILE = ARGS.unseen_event_file
 EMBEDDING_FILE = ARGS.embedding_file
-CORPUS_FILE = ARGS.corpus_file 
+CORPUS_FILE = ARGS.corpus_file
 jieba.set_dictionary("./jieba-zh_TW/jieba/dict.txt")
 def vsm(fp=CORPUS_FILE):
     with open(fp, 'r') as json_file_in:
@@ -36,11 +37,11 @@ def vsm(fp=CORPUS_FILE):
         vectorizer = TfidfVectorizer()
         document_term_matrix = vectorizer.fit_transform(corpus)
         dim = document_term_matrix.shape[1]
-        annoy_index = AnnoyIndex(dim, 'euclidean')
+        annoy_index = AnnoyIndex(dim)
         for index, vector in enumerate(document_term_matrix):
             annoy_index.add_item(index, vector.toarray()[0])
         annoy_index.build(10) # 10 trees
-        annoy_index.save('vsm_euclidean.ann')
+        annoy_index.save('vsm_tfidf.ann')
         return index_id_dict, vectorizer, document_term_matrix
 
 def closest_topK(unseen_event, ids_dict, model, dim, topK=10):
@@ -48,20 +49,21 @@ def closest_topK(unseen_event, ids_dict, model, dim, topK=10):
     unseen_event_vector = [0] * dim
     for tag in unseen_even_tags:
         if tag in model.vocabulary_:
-            unseen_event_vector[model.vocabulary_[tag]] += 1
-    annoy_index = AnnoyIndex(dim, 'euclidean')
-    annoy_index.load('vsm_euclidean.ann')
+            unseen_event_vector[model.vocabulary_[tag]] += model.idf_[model.vocabulary_[tag]]
+    annoy_index = AnnoyIndex(dim)
+    annoy_index.load('vsm_tfidf.ann')
     ranking_list = annoy_index.get_nns_by_vector(unseen_event_vector, 10, search_k=-1, include_distances=True)
     propgation_list = []
     for matrix_row, score in zip(ranking_list[0], ranking_list[1]):
         propgation_list.append((ids_dict[matrix_row], score))
     return propgation_list
 
-def embedding_propgation(ranking_list, fp=EMBEDDING_FILE):
+def embedding_propgation(ranking_list, weight_func = lambda x : 1, fp=EMBEDDING_FILE):
     with open(EMBEDDING_FILE, 'r') as json_file_in:
         embedding_dict = json.load(json_file_in)
     accumulate_vector = []
     accumulate_weight = 0
+    weight_list = []
     add_count = 0
     for ranking_list_index, (id_, score) in enumerate(ranking_list):
         try:
@@ -71,14 +73,17 @@ def embedding_propgation(ranking_list, fp=EMBEDDING_FILE):
             # they are removed from the training set.
             print("{} is not a significant event so that not included in the training embedding.".format(id_))
             continue
+        weight = weight_func(score)
+        weight_list.append(weight)
         if add_count == 0:
-            accumulate_vector = list(map(lambda x: x * (1 / (0.00001 + score)), added_vector))
+            accumulate_vector = list(map(lambda x: x * weight, added_vector))
         else:
             for index, (element1, element2) in\
                             enumerate(zip(accumulate_vector, added_vector)):
-                accumulate_vector[index] = element1 + element2 * (1 / (0.00001 + score))
+                accumulate_vector[index] = element1 + element2 * weight
         add_count += 1
-        accumulate_weight += 1 / (0.00001 + score)
+        accumulate_weight += weight
+    print('weight list: {}'.format(list(map(lambda x: x / accumulate_weight, weight_list))))
     print('{} related events.'.format(add_count))
     return list(map(lambda x: x / accumulate_weight, accumulate_vector))
 
@@ -103,9 +108,9 @@ if __name__ == "__main__":
         ID_LIST =\
             closest_topK(title_string, IDS_DICT, TRAINED_MODEL, DOC_MATRIX.shape[1])
         print(ID_LIST)
-        UNSEEN_EMBEDDING_DICT[id_] = embedding_propgation(ID_LIST)
+        UNSEEN_EMBEDDING_DICT[id_] = embedding_propgation(ID_LIST, weight_func=lambda x : 1 / ((acos(x / -2 + 1) * 180 / pi) ** 2) if x != 0 else 1)
         print()
-    with open('unssen_events_rep_hpe(tf_weight_euclidean).txt', 'wt') as fout:
+    with open('unssen_events_rep_hpe(tfidf_degWeight_angular).txt', 'wt') as fout:
         fout.write("{}\n".format(len(UNSEEN_EMBEDDING_DICT)))
         for id_, embedding in UNSEEN_EMBEDDING_DICT.items():
             fout.write("{} {}\n".format(id_, ' '.join(map(lambda x:str(round(x, 6)),embedding))))
