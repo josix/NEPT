e6c1d4412b274e43ef0ef7e6aac55521cf4dccd9
lhenry15 <khlai@gmail.com>
2018-11-05 19:12:16 +0800

    add src folder

--- run_label_embedding.sh
+++ run_label_embedding.sh
@@ -17,8 +17,13 @@ fi
 # Generate user-item graph
 ./script/extract.sh $1 $2 $3 $4
 python3 ./script/generate_item_list.py -o ./data/itemsList.data ./data/user-item.data
-python3 ./script/data_split.py -o1 ./data/train.data -o2 ./data/test.data ./data/itemsList.data
-python3 "./script/export.py" -o "./data/export.data" ./data/train.data
+
+# random separate data
+#python3 ./script/data_split.py -o1 ./data/train.data -o2 ./data/test.data ./data/itemsList.data
+
+# generate network data for training hpe  
+python3 "./script/export.py" -o "./data/export.data" ./data/itemsList.data
+
 # HPE trainning
 ../proNet-core/cli/hpe -train ./data/export.data -save ./data/rep.hpe -undirected 1 -dimensions 128 -reg 0.01 -sample_times 5 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads 4
 # Turn word2vec format into JSON
@@ -33,4 +38,4 @@ python3 ./script/construct_user_word_graph.py -o ./data/user-label.data ./data/e
 # Train line-2nd on user-word graph
 ../proNet-core/cli/line -train ./data/user-label.data  -save ./data/textrank/rep.line2 -undirected 1 -order 2 -dimensions 128 -sample_times 4 -negative_samples 5 -alpha 0.025 -threads 4
 # Propagate HPE embedding based on user-word graph
-python3 ./experiment/label_propagation.py $5 ./data/rep.json ./data/textrank/textrank.json ./data/textrank
+python3 ./src/label_propagation.py $5 ./data/rep.json ./data/textrank/textrank.json ./data/textrank
