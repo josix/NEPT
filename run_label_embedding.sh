3398c716f963c1b745434bbc1e8f00ce2ad9cf36
WilsonWang <wilson8507@gmail.com>
2018-11-05 18:52:04 +0800

    Add comments in run_label_embedding

--- run_label_embedding.sh
+++ run_label_embedding.sh
@@ -14,15 +14,23 @@ then
   exit 1
 fi
 
+# Generate user-item graph
 ./script/extract.sh $1 $2 $3 $4
 python3 ./script/generate_item_list.py -o ./data/itemsList.data ./data/user-item.data
 python3 ./script/data_split.py -o1 ./data/train.data -o2 ./data/test.data ./data/itemsList.data
 python3 "./script/export.py" -o "./data/export.data" ./data/train.data
+# HPE trainning
 ../proNet-core/cli/hpe -train ./data/export.data -save ./data/rep.hpe -undirected 1 -dimensions 128 -reg 0.01 -sample_times 5 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads 4
+# Turn word2vec format into JSON
 python3 ./script/rep_transform.py -o ./data/rep.json ./data/rep.hpe
+# Segement title
 python3 ./script/segement.py -o ./data/tags.json ./data/eventsTitle.data
 mkdir data/textrank
+# Generate keywords form title and description
 python3 ./script/textrank.py -o ./data/textrank/textrank  ./data/eventsTitle.data
+# Construct user-label(word) graph
 python3 ./script/construct_user_word_graph.py -o ./data/user-label.data ./data/export.data ./data/textrank/textrank.json ./data/textrank/textrank_mapping.txt
+# Train line-2nd on user-word graph
 ../proNet-core/cli/line -train ./data/user-label.data  -save ./data/textrank/rep.line2 -undirected 1 -order 2 -dimensions 128 -sample_times 4 -negative_samples 5 -alpha 0.025 -threads 4
+# Propagate HPE embedding based on user-word graph
 python3 ./experiment/label_propagation.py $5 ./data/rep.json ./data/textrank/textrank.json ./data/textrank
