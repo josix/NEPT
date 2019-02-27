ab59f6a667b0e7d3d5c005577aa8fe6075bac7ec
WilsonWong <wilson8507@gmail.com>
2019-02-27 14:37:16 +0800

    Update preprocessing script to flexible path

--- preprocessing.sh
+++ preprocessing.sh
@@ -26,28 +26,34 @@ awk '{print $2}' $TARGET_SOURCE/user-item.data | sort -u | awk '{print "^"$0","}
 grep -f $IM_DIR/training_event_pattern.txt $IM_DIR/events_description_v7.data> $TARGET_SOURCE/events.csv
 
 # Generate testing query data
-mkdir ./experiment/data
-mkdir ./experiment/data/precision
+EXP_QUERY_DIR="/path/to/store/exp/query/setting" # need to sync with experiment script
+# EXP_QUERY_DIR="./experiment/data"
+mkdir $EXP_QUERY_DIR
 awk -f ./preprocessing/user_to_items.awk $IM_DIR/user-item-frequency.data > $IM_DIR/user_to_items_training.data
 cut -f 2,3 -d, $IM_DIR/entertainment_transactions_v7_Before20161231.data | sort -t, -k 2 | uniq -c | sort -r -k 1 -n| head -n 500 > $IM_DIR/popular_events_with_count.txt
 awk '{$1=""; print $0}' $IM_DIR/popular_events_with_count.txt| head -n 100 > $IM_DIR/popular_events_100.txt
-python3 ./experiment/query_generator/precision_query_generator.py $IM_DIR/user_to_items_training.data $IM_DIR/popular_events_100.txt > ./experiment/data/precision@5_1user_1item_top100_popular_query_user_click_10.txt
+python3 ./experiment/query_generator/precision_query_generator.py $IM_DIR/user_to_items_training.data $IM_DIR/popular_events_100.txt > $EXP_QUERY_DIR/precision@5_1user_1item_top100_popular_query_user_click_10.txt
 
 # Generate testing (unseen) events data
 cat $RAW_DATA_DIR/entertainment_events_20180523.csv $RAW_DATA_DIR/entertainment_events_20180903.csv $RAW_DATA_DIR/entertainment_events_v6.csv $RAW_DATA_DIR/entertainment_events_v7.csv | sort -u > $IM_DIR/entertainment_events_all.csv
 python3 ./preprocessing/parser.py --testing 1 $IM_DIR/entertainment_events_all.csv > $IM_DIR/unseen_2018_events_description.csv
 
 # Treat the query item as unseen item
-cut -f 2 -d ' '  ./experiment/data/precision@5_1user_1item_top100_popular_query_user_click_10.txt | sort -u > $IM_DIR/query_item.data
+cut -f 2 -d ' '  $EXP_QUERY_DIR/precision@5_1user_1item_top100_popular_query_user_click_10.txt | sort -u > $IM_DIR/query_item.data
 grep -f $IM_DIR/query_item.data $TARGET_SOURCE/events.csv >  $IM_DIR/query_item_description.data
 cat $IM_DIR/unseen_2018_events_description.csv $IM_DIR/query_item_description.data > $TARGET_SOURCE/unseen_2018_events_description.csv
 
 # Generate testing answer data
+EXP_ANSWER_DIR="/path/to/store/exp/answer/setting" # need to sync with experiment script
+# EXP_ANSWER_DIR="./experiment/data/precision"
+mkdir $EXP_ANSWER_DIR
 awk -f ./preprocessing/extract_user_item_frequency_2018.awk $RAW_DATA_DIR/entertainment_transactions_20180903.csv > $IM_DIR/user-item-2018.data
-awk -f ./preprocessing/user_to_items.awk $IM_DIR/user-item-2018.data > ./experiment/data/precision/transaction_future_answer.data
+awk -f ./preprocessing/user_to_items.awk $IM_DIR/user-item-2018.data > $EXP_ANSWER_DIR/transaction_future_answer.data
 
 # Generate metadata
-mkdir -p ../kktix/preproecessed_data
-awk -F, '{OFS=","; print $1, $4, $5}' $RAW_DATA_DIR/entertainment_events_20180903.csv | sort -u > ../kktix/preproecessed_data/eventDetailMap_20180903.csv
-awk -F, '{OFS=","; print $1, $4, $5}' $RAW_DATA_DIR/entertainment_events_20180523.csv | sort -u > ../kktix/preproecessed_data/eventDetailMap_20180523.csv
-awk -F, 'BEGIN{OFS=","} {print $11,$12,$13}' $RAW_DATA_DIR/entertainment_transactions_v7.csv | sort | uniq  > ../kktix/preproecessed_data/eventDetailMap_v7.csv
+META_DIR="/path/to/store/metadata" # need to sync with experiment script
+# META_DIR="../kktix/preproecessed_data"
+mkdir -p $META_DIR
+awk -F, '{OFS=","; print $1, $4, $5}' $RAW_DATA_DIR/entertainment_events_20180903.csv | sort -u > $META_DIR/eventDetailMap_20180903.csv
+awk -F, '{OFS=","; print $1, $4, $5}' $RAW_DATA_DIR/entertainment_events_20180523.csv | sort -u > $META_DIR/eventDetailMap_20180523.csv
+awk -F, 'BEGIN{OFS=","} {print $11,$12,$13}' $RAW_DATA_DIR/entertainment_transactions_v7.csv | sort | uniq  > $META_DIR/eventDetailMap_v7.csv
