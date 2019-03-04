dc4f9cf0232eec82090530c6c956f20e64d6cb42
WilsonWong <wilson8507@gmail.com>
2019-03-04 11:01:53 +0800

    Update run_label_embedding script to take arguments

--- run_label_embedding.sh
+++ run_label_embedding.sh
@@ -1,44 +1,28 @@
 # /bin/sh
-# USAGE="usage: run.sh training_file_path userId_column_number itemId_column_number title_column_number unseen_events_file_path"
-# test "$#" != "5" && echo "$USAGE" && exit 1
-# 
-# if ! [ -e $1 ]
-# then
-#   echo "$USAGE\nFile \"$1\" is not exists"
-#   exit 1
-# fi
-# 
-# if ! [ -f $1 ]
-# then
-#   echo "$USAGE\n\"$1\" is not a file"
-#   exit 1
-# fi
+USAGE="usage: run_label_embedding.sh source target_dir\n\nsource:\tThe directory of the training data\ntarget_dir:\tThe directory to store trained data"
+test "$#" != "2" && echo "$USAGE" && exit 1
 
-# Generate user-item graph
-# ./script/extract.sh $1 $2 $3 $4
-# python3 ./script/generate_item_list.py -o ./data/itemsList.data ./data/user-item.data
+if ! [ -e $1 ]
+then
+  echo "$USAGE\nDirectroy \"$1\" is not exists"
+  exit 1
+fi
+SOURCE_DIR=$1
+TARGET_DIR=$2
 
-# random separate data
-#python3 ./script/data_split.py -o1 ./data/train.data -o2 ./data/test.data ./data/itemsList.data
-
-# generate network data for training hpe
-# python3 "./script/export.py" -o "./data/export.data" ./data/itemsList.data
-
-mkdir data
+mkdir $TARGET_DIR
 # HPE trainning
-./proNet-core/cli/hpe -train ./source/user-item.data -save ./data/rep.hpe -undirected 1 -dimensions 128 -reg 0.01 -sample_times 5 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads 20
+./proNet-core/cli/hpe -train $SOURCE_DIR/user-item.data -save $TARGET_DIR/rep.hpe -undirected 1 -dimensions 128 -reg 0.01 -sample_times 5 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads 20
 # Turn word2vec format into JSON
-python3 ./script/rep_transform.py -o ./data/rep.json ./data/rep.hpe
-# Segement title
-# python3 ./script/segement.py -o ./data/tags.json ./data/eventsTitle.data
-mkdir data/textrank
+python3 ./script/rep_transform.py -o $TARGET_DIR/rep.json $TARGET_DIR/rep.hpe
+mkdir $TARGET_DIR/textrank
 # Generate keywords form title and description
-python3 ./script/textrank.py -o ./data/textrank/textrank  ./source/events.csv
+python3 ./script/textrank.py -o $TARGET_DIR/textrank/textrank  $SOURCE_DIR/events.csv
 # Construct user-label(word) graph
-python3 ./script/construct_user_word_graph.py -o ./data/textrank/user-label.data ./source/user-item.data ./data/textrank/textrank.json ./data/textrank/textrank_mapping.txt
+python3 ./script/construct_user_word_graph.py -o $TARGET_DIR/textrank/user-label.data $SOURCE_DIR/user-item.data $TARGET_DIR/textrank/textrank.json $TARGET_DIR/textrank/textrank_mapping.txt
 # Train line-2nd on user-word graph
-./proNet-core/cli/line -train ./data/textrank/user-label.data  -save ./data/textrank/rep.line2 -undirected 1 -order 2 -dimensions 128 -sample_times 40 -negative_samples 5 -alpha 0.025 -threads 20
+./proNet-core/cli/line -train $TARGET_DIR/textrank/user-label.data  -save $TARGET_DIR/textrank/rep.line2 -undirected 1 -order 2 -dimensions 128 -sample_times 40 -negative_samples 5 -alpha 0.025 -threads 20
 # Generate semantic space embedding
-python3 ./src/label_propagation.py ./source/unseen_2018_events_description.csv ./data/rep.json ./data/textrank/textrank.json ./data/textrank
+python3 ./src/label_propagation.py $SOURCE_DIR/unseen_2018_events_description.csv $TARGET_DIR/rep.json $TARGET_DIR/textrank/textrank.json $TARGET_DIR/textrank
 # Generate preference space embedding
-python3 ./src/vsm_propagation.py ./source/unseen_2018_events_description.csv ./data/rep.json  ./data/textrank/textrank.json
+python3 ./src/vsm_propagation.py $SOURCE_DIR/unseen_2018_events_description.csv $TARGET_DIR/rep.json  $TARGET_DIR/textrank/textrank.json
